f72317400948e223610dcdac8032e3461f4080a3
Pedro Augusto <109158387+avaias@users.noreply.github.com>
2023-08-11 12:21:12 -0300

    Commit inicial do projeto

---
+++ AtividadeAvaliativa02/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ExercicioUmScreen from './src/screens/ExercicioUmScreen';
+
+export default function App() {
+  return (
+    /*
+    <View style = {styles.container}>
+      <Text style = {styles.textBox}>App</Text>
+    </View>
+    */
+   <ExercicioUmScreen/>
+  );
+}
